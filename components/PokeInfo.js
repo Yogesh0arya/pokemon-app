2f137d65187d064e56afb7ce25c7b7d3732ffb5c
Yogesh0arya <yogesh084arya@gmail.com>
2022-04-20 22:34:24 +0530

    ignore eslint rule

--- components/PokeInfo.js
+++ components/PokeInfo.js
@@ -17,18 +17,21 @@ function PokeInfo({ data }) {
             alt="img"
           />
           <div className="flex gap-2 flex-wrap">
-            {data.abilities.map((poke) => {
+            {data.abilities.map((poke, i) => {
               return (
-                <h2 className="px-2 py-1 text-white bg-blue-400 rounded-lg">
+                <h2
+                  key={i}
+                  className="px-2 py-1 text-white bg-blue-400 rounded-lg"
+                >
                   {poke.ability.name}
                 </h2>
               );
             })}
           </div>
           <div>
-            {data.stats.map((poke) => {
+            {data.stats.map((poke, i) => {
               return (
-                <h3>
+                <h3 key={id}>
                   {poke.stat.name}: {poke.base_stat}
                 </h3>
               );
